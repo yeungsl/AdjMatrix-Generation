6fc91d69b618c1264118ff59eea57e4f46ea2731
Sailung Yeung <yeungSL@Sailungs-MacBook-Pro.local>
2017-06-12 23:26:25 -0400

    added a basic infrastracture

--- GTI_APP.py
+++ GTI_APP.py
@@ -1,27 +1,40 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+Author: Sailung Yeung <yeungsl@bu.edu>
+"""
 import os
 import sys
+import argparse
+import networkx as nx
 import Conditional_Topology_MAIN as CTM
 from external_tools.community_louvain import generate_dendrogram, partition_at_level
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", type=str, default="facebook", help="Datasets Choose")
+    parser.add_argument("--noise", type=int, default=10, help="ignor the community that is has size smaller than this nubmer")
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    filename = sys.argv[1]
+    args = parse_args()
+    filename = args.filename
+    noise = args.noise
     path = os.path.join('data', filename, '')
     graph = CTM.generate_graph(path, filename, -1)
     print("got graph from CTM with", len(graph.nodes()), "nodes and", len(graph.edges()),"edges......")
     dendrogram = generate_dendrogram(graph)
-    print("length of the dendrogram generated is", len(dendrogram))
-    for level in range(len(dendrogram)):
-        stats = dict()
-        cur_layer = partition_at_level(dendrogram, level)
-        for node in cur_layer.keys():
-            if cur_layer[node] in stats.keys():
-                stats[cur_layer[node]] += 1
-            else:
-                stats[cur_layer[node]] = 1
+    print("length of the dendrogram generated is", len(dendrogram), "we are using the best layer by defealt")
+    best = dendrogram[len(dendrogram) - 1]
+    communities = {}
 
-        avg = len(graph.nodes()) / len(stats.keys())
-        print("for layer", level, "we have", len(stats.keys()), "and avg", avg, "that has stats", stats)
-        if avg >= 4500 and avg <= 5000:
-            print("found the level that has around 5000 nodes in one community")
-            print("the partition is at level", level, "that has stats", stats)
-            break
+    for node in best.keys():
+        com_num = best[node]
+        if com_num not in communities.keys():
+            communities[com_num] = [node]
+        else:
+            communities[com_num].append(node)
+
+    for com_num in communities.keys():
+        if len(communities[com_num]) >= noise:
+            com = graph.subgraph(communities[com_num])
